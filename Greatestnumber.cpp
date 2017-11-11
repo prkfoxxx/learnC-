7dc604cd789ef5ff8e1b122e6c5641efe93c77e2
PRIYANSH-SYSTEM\PIKU <priyansh.singal001@gmail.com>
2017-11-11 23:59:22 +0530

    Starting from simple...Greatest Number among three integer values.......

---
+++ Greatestnumber.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+void gon(int x, int y, int z);
+
+int main() {
+
+int a, b, c;
+cout << "Enter the values of all three numbers:" << endl;
+
+cin >> a;
+cin >> b;
+cin >> c;
+
+void gon (int a, int b, int c) ;
+return 0;
+}
+
+void gon (int x, int y, int z) {
+
+if( x > y){
+    if (x > z) {
+        cout << "The greatest value is: " <<  x ;
+    }
+    else {
+        cout << "The Greatest number is: " << z ;
+    }
+}
+if (y > x) {
+    if(y >z){
+        cout << "Gretest number is :" << y << endl ; }
+
+    else {
+        cout << "Greatest number is :" << z << endl; }
+}
+
+
+  }
