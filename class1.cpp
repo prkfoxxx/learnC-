ad55371c2ebadf5b82bc8d9bca1d25924fc29c50
Priyansh Kumar <priyansh.singhal001@gmail.com>
2018-01-13 02:38:51 +0530

    This the another simple student class example

---
+++ class1.cpp
@@ -0,0 +1,35 @@
+#include <iostream.h>       //header file
+#include <conio.h>          // header file 
+
+using namespace std;
+
+class Student {                             // class named student is defined .
+public:
+    
+    string name[10];
+    int age;
+    
+    void read(){
+        
+        cout << "Enter the name of the student" ;
+        cin >> name;
+        
+        cout << "Enter the age of the student";
+        cin >> age ;
+        
+    }
+    
+    void print(){
+        
+        cout << "Name of the student is : %s" << name ;         // print the name of the student
+        
+        cout << " Age of the student is: %d" << age ;           // print the age of the student
+    }
+};
+
+void main(){
+    
+    Student.s;  // object of the class is defined
+    
+    s.read();
+}
