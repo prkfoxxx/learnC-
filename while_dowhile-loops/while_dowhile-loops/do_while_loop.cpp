e342f7979093e01f2fb902fb8bdc4e62309cbd7a
Priyansh Kumar <priyansh.singhal001@gmail.com>
2018-01-18 23:03:07 +0530

    This is a demo code on while loop and do while loop ..

---
+++ while_dowhile-loops/while_dowhile-loops/do_while_loop.cpp
@@ -0,0 +1,22 @@
+//
+//  do_while_loop.cpp
+//  while_dowhile-loops
+//
+//  Created by Priyansh Kumar on 18/01/18.
+//  Copyright © 2018 Priyansh Kumar. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i=1;
+    
+    do {
+        cout<<"This is a demo code";
+        
+    } while(i<5);
+    
+    return 0;
+    
+}
---
+++ while_dowhile-loops/while_dowhile-loops/main.cpp
@@ -0,0 +1,26 @@
+//
+//  main.cpp
+//  while_dowhile-loops
+//
+//  Created by Priyansh Kumar on 18/01/18.
+//  Copyright © 2018 Priyansh Kumar. All rights reserved.
+//
+
+#include <iostream>      //Infinitive While Loop Example
+
+using namespace std;
+
+int main(){
+    int a,i=0;
+    
+    cout <<"Ente the value of a:";
+    cin >> a;
+    
+    while (a!=0){
+        cout<<"This is the first step "<<i;
+        i++;
+        
+    }
+    return 0;
+    
+}
