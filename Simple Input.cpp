aa15afb366dedaff7dc304135f0494d094a78878
Priyansh Kumar <priyansh.singhal001@gmail.com>
2019-10-08 10:06:19 +0530

    File is related to showing input untill some becomes negative.

---
+++ Simple Input.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int num,sum=0;
+    while (sum>=0){
+        cin>>num;
+        
+        sum+=num;
+        if(sum>=0){
+            cout<<num<<endl;}
+        
+    }
+    return 0;
+}
